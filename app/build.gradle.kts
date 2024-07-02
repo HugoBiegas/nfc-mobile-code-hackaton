95538b24ee8946c32aab715a66d8f5cfae99744a
Hugo <78080239+HugoBiegas@users.noreply.github.com>
2024-07-02 22:24:19 +0200

    okhttp3 supprimer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
 
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
